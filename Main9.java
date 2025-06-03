8de41ec045b88ba6bcbc9f4d9ea04d0634927ad1
devisetty435 <ddevi7028@gmail.com>
2025-06-03 15:51:45 +0530

    Add files via upload

---
+++ Main9.java
@@ -0,0 +1,12 @@
+import java.util.Stack;
+public class Main9{
+    public static void main(String[] args){
+        Stack s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        System.out.println(s.pop());
+        System.out.println(s.peek());
+        
+    }
+
+}
